969306a98046c98523652ef2a9d7b2fdc714ec5f
DakotaLMartinez <dakotaleemusic@gmail.com>
2021-01-03 11:44:30 -0800

    end of day1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  use_doorkeeper
+  devise_for :users
+  resources :businesses
+  root 'businesses#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications, :authorized_applications
+  end
 end
