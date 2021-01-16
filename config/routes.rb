5d20ed42ca2187008fd8be2a62f5c84274e2b931
DakotaLMartinez <dakotaleemusic@gmail.com>
2021-01-16 00:04:41 -0800

    add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   use_doorkeeper
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   resources :businesses
   root 'businesses#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
