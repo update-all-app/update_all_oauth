c59a32b3be96ae86258fe51cd67f2d37947361ef
micahshute <micah.shute@gmail.com>
2021-05-01 21:39:37 -0700

    adds update splitter method and implements

--- app/services/multi_day_event_splitter_service.rb
+++ app/services/multi_day_event_splitter_service.rb
@@ -1,5 +1,5 @@
 class MultiDayEventSplitterService 
-  def self.process(irregular_event)
+  def self.process_create(irregular_event)
     start_date = irregular_event.start_time.to_date
     end_date = irregular_event.end_time.to_date
     if irregular_event.status == "closed" && start_date != end_date
@@ -11,6 +11,24 @@ class MultiDayEventSplitterService
         )
       end
     else 
+      [irregular_event]
+    end
+  end
+
+  def self.process_update(irregular_event)
+    start_date = irregular_event.start_time.to_date
+    end_date = irregular_event.end_time.to_date
+    if irregular_event.status == 'closed' && start_date != end_date
+      irregular_event.end_time = UtcDatetimeService.utc_from_date(start_date, start_of_day: false)
+      events = (start_date.next_day..end_date).map do |day|
+        irregular_event.clone(
+          status: 'closed',
+          start_time: UtcDatetimeService.utc_from_date(day, start_of_day: true),
+          end_time: UtcDatetimeService.utc_from_date(day, start_of_day: false)
+        )
+      end
+      [irregular_event] + events
+    else
       [irregular_event]
     end
   end
